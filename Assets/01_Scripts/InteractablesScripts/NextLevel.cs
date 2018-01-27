1ff771afe80b7b1f61d740cf34a0681179336fe3
zafoof <bsule321@gmail.com>
2018-01-27 18:04:05 -0500

    git fixes

--- Assets/01_Scripts/InteractablesScripts/NextLevel.cs
+++ Assets/01_Scripts/InteractablesScripts/NextLevel.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
-<<<<<<< HEAD:Assets/01_Scripts/InteractablesScripts/NextLevel.cs
+
 public class NextLevel : MonoBehaviour {
 
 	public int sceneTransitionIndex;
@@ -11,12 +11,5 @@ public class NextLevel : MonoBehaviour {
     private void OnTriggerEnter2D(Collider2D collision)
     {
 		SceneManager.LoadScene (sceneTransitionIndex);
-=======
-public class NextLevel : MonoBehaviour {
-
-    private void OnTriggerEnter2D(Collider2D collision)
-    {
-        SceneManager.LoadScene((SceneManager.GetActiveScene().buildIndex%2)+1);
->>>>>>> c4e6adef46cf5b088cf0aee3a0fd4ca6e4e6a87d:Assets/NextLevel.cs
     }
 }
