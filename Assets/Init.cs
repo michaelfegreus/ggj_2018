39e72ca4e4517facc706dc54fe217e5eab62400d
zafoof <bsule321@gmail.com>
2018-01-27 17:51:51 -0500

    remove duplicate

--- Assets/Init.cs
+++ Assets/Init.cs
@@ -3,7 +3,8 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
-public class Init : MonoBehaviour{
+public class Init : MonoBehaviour
+{
     public static Init instance = null;
     private static bool first = true;
     public static int num_scenes = 3;
@@ -13,12 +14,12 @@ public class Init : MonoBehaviour{
     private void Awake()
     {
         FirstRun();
-        for(int j = 0; j < events[SceneManager.GetActiveScene().buildIndex]; j++)
+        for (int j = 0; j < events[SceneManager.GetActiveScene().buildIndex]; j++)
         {
             if (!activezones[SceneManager.GetActiveScene().buildIndex][j])
             {
                 GameObject.Find("CellZone" + j).SetActiveRecursively(false);
-            }                
+            }
         }
     }
     void FirstRun()
@@ -36,30 +37,5 @@ public class Init : MonoBehaviour{
             first = false;
         }
 
-    private void Awake()
-    {
-        FirstRun();
-        for(int j = 0; j < events[SceneManager.GetActiveScene().buildIndex]; j++)
-        {
-            if (!activezones[SceneManager.GetActiveScene().buildIndex][j])
-            {
-                GameObject.Find("CellZone" + j).SetActiveRecursively(false);
-            }                
-        }
-    }
-    void FirstRun()
-    {
-        if (first)
-        {
-            for (int i = 0; i < num_scenes; i++)
-            {
-                activezones[i] = new bool[events[i]];
-                for (int j = 0; j < events[i]; j++)
-                {
-                    activezones[i][j] = true;
-                }
-            }
-            first = false;
-        }
     }
 }
